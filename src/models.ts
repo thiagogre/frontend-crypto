38a6a8434fbde76a326343e412268a2289bf00f6
Thiago Gregorio <thiago.gregorio@t2software.com.br>
2021-07-25 16:11:51 -0300

    feat added new types

--- src/models.ts
+++ src/models.ts
@@ -5,9 +5,31 @@ export type Credentials = {
     secretKey: string;
 };
 
+export type UserCredentials = {
+    list: Credentials[] | [];
+    current: Credentials | null;
+};
+
+export type Language = 'en' | 'pt';
+
 export type Route = {
     exact: boolean;
     path: string;
     name: string;
     component: React.FC<{}>;
+};
+
+export type Coins = {
+    asset: string;
+    price: number;
+    free: number;
+    value: number;
+    locked: number;
+};
+
+export type AllCoinsResponse = {
+    prices: {
+        totalAssetUSD: number;
+        coinPrices: Coins[];
+    };
 };
