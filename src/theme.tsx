58c4adc9a28dfadeb20877c15bb9bbf7f2e152ca
Thiago Gregorio <thiago.gregorio@t2software.com.br>
2021-07-25 01:01:40 -0300

    refactor theme

---
+++ src/theme.tsx
@@ -0,0 +1,33 @@
+import { ReactNode } from 'react';
+import { createMuiTheme, ThemeProvider, colors } from '@material-ui/core';
+import { PaletteOptions } from '@material-ui/core/styles/createPalette';
+
+type ThemeProps = {
+    type: 'dark' | 'light';
+    children: ReactNode;
+};
+
+export const ApplicationThemeProvider: React.FC<ThemeProps> = ({
+    type,
+    children,
+}) => {
+    const palette: PaletteOptions = {
+        type,
+        primary: {
+            light: colors.orange['A200'],
+            main: colors.orange['A400'],
+            dark: colors.orange['A700'],
+            contrastText: '#000',
+        },
+        secondary: {
+            light: colors.grey['A200'],
+            main: colors.grey['A400'],
+            dark: colors.grey['A700'],
+            contrastText: '#000',
+        },
+    };
+
+    const theme = createMuiTheme({ palette });
+
+    return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
