6d515d7ec1d1a9b6204bfb56b83219df80a72f6a
Thiago Gregorio <thiago.gregorio@t2software.com.br>
2021-08-21 21:48:17 -0300

    feat added pie chart component

---
+++ src/components/charts/Pie/index.tsx
@@ -0,0 +1,58 @@
+import { ResponsivePie } from '@nivo/pie';
+// make sure parent container have a defined height when using
+
+type Data = {
+    id: string;
+    value: number;
+};
+
+type Props = {
+    data: Data[] | undefined;
+    colors: string[];
+};
+export const PieChartComponent: React.FC<Props> = ({ data, colors }) => {
+    return data ? (
+        <ResponsivePie
+            data={data}
+            colors={colors}
+            margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+            innerRadius={0.5}
+            padAngle={0.7}
+            cornerRadius={3}
+            activeOuterRadiusOffset={8}
+            borderWidth={1}
+            borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
+            arcLinkLabelsSkipAngle={10}
+            arcLinkLabelsTextColor="#333333"
+            arcLinkLabelsThickness={2}
+            arcLinkLabelsColor={{ from: 'color' }}
+            arcLabelsSkipAngle={10}
+            arcLabelsTextColor={{
+                from: 'color',
+                modifiers: [['darker', 2]],
+            }}
+            defs={[
+                {
+                    id: 'dots',
+                    type: 'patternDots',
+                    background: 'inherit',
+                    color: 'rgba(255, 255, 255, 0.3)',
+                    size: 4,
+                    padding: 1,
+                    stagger: true,
+                },
+                {
+                    id: 'lines',
+                    type: 'patternLines',
+                    background: 'inherit',
+                    color: 'rgba(255, 255, 255, 0.3)',
+                    rotation: -45,
+                    lineWidth: 6,
+                    spacing: 10,
+                },
+            ]}
+        />
+    ) : (
+        <></>
+    );
+};
